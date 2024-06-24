f3f0aaaee6d99949db68a3705c634c9ec7d2f20f
smpaliya <137785961+smpaliya@users.noreply.github.com>
2024-06-24 22:45:15 +0530

    Add files via upload

---
+++ frontend/Account.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './Page1.css';
+export default function Account(){
+    
+    return(
+<>
+<div className='main'>
+<p>Account</p>
+</div>
+</>
+
+    )
+}
